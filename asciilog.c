4d4ed5b79ebc5496723fa82ad35e83cb9d6cc21b
Dima Kogan <Dmitriy.Kogan@jpl.nasa.gov>
2016-09-07 11:32:15 -0700

    We can now log to non-global contexts
    This is an architectural change that breaks the ABI (but not the API).
    Previously we would only log to a single global context, and we could save a
    record-in-progress with asciilog_stash(). This was completely unworkable in a
    multi-threaded system where multiple records could be constructed at the same
    time.
    
    So we no have the previous API functions that refer to the global context and
    also ..._ctx flavors that refer to a particular context. The idiom is
    
        asciilog_context_t ctx;
        asciilog_context_clear_ctx(&ctx);
        asciilog_set_field_value_ctx__x(5);
        asciilog_set_field_value_ctx__y(6);
        ....
        asciilog_emit_record_ctx(&ctx);
    
    If threading, each thread would have its own context, and
    asciilog_emit_record_ctx() knows not to mix incoming records.
    
    This change also obviates the need for the stash() calls, and they have been
    removed.

--- asciilog.c
+++ asciilog.c
@@ -10,12 +10,18 @@
   exit(1);                                                              \
 } while(0)
 
-static FILE*             fp                  = NULL;
-static bool              legend_finished     = false;
-static bool              line_has_any_values = false;
-static asciilog_field_t* fields              = NULL;
+static FILE* fp = NULL;
+static bool legend_finished;
 
+// ASCIILOG_N_FIELDS is unknown here so the asciilog_context_t structure has 0
+// elements. I dynamically allocate it later with the proper size
+static struct asciilog_context_t* global_context;
 
+__attribute__((constructor))
+static void init(void)
+{
+    global_context = calloc(1, sizeof(struct asciilog_context_t));
+}
 
 static void check_fp(void)
 {
@@ -63,13 +69,15 @@ static void flush(void)
     fflush(fp);
 }
 
-static void clear(int Nfields)
+void _asciilog_clear_ctx(struct asciilog_context_t* ctx, int Nfields)
 {
-    line_has_any_values = false;
+    if( ctx == NULL ) ctx = global_context;
+
+    ctx->line_has_any_values = false;
     for(int i=0; i<Nfields; i++)
     {
-        fields[i].c[0] = '-';
-        fields[i].c[1] = '\0';
+        ctx->fields[i].c[0] = '-';
+        ctx->fields[i].c[1] = '\0';
     }
 }
 
@@ -81,41 +89,48 @@ void _asciilog_emit_legend(const char* legend, int Nfields)
     emit(legend);
     flush();
 
-    fields = malloc(Nfields * sizeof(fields[0]));
-    if(!fields)
-        ERR("Couldn't allocate %d fields", Nfields);
+    global_context = realloc(global_context,
+                             sizeof(struct asciilog_context_t) +
+                             Nfields * sizeof(global_context->fields[0]));
+    if(!global_context)
+        ERR("Couldn't allocate context with %d fields", Nfields);
     legend_finished = true;
 
-    clear(Nfields);
+    _asciilog_clear_ctx(global_context, Nfields);
 }
 
-void _asciilog_set_field_value(const char* fieldname, int idx,
+void _asciilog_set_field_value(struct asciilog_context_t* ctx,
+                               const char* fieldname, int idx,
                                const char* fmt, ...)
 {
+    if( ctx == NULL ) ctx = global_context;
+
     if(!legend_finished)
         ERR("need a legend to do this");
-    if(fields[idx].c[0] != '-' || fields[idx].c[1] != '\0')
+    if(ctx->fields[idx].c[0] != '-' || ctx->fields[idx].c[1] != '\0')
         ERR("Field '%s' already set. Old value: '%s'",
-            fieldname, fields[idx].c);
+            fieldname, ctx->fields[idx].c);
 
-    line_has_any_values = true;
+    ctx->line_has_any_values = true;
 
     va_list ap;
     va_start(ap, fmt);
-    if( (int)sizeof(fields[0]) <=
-        vsnprintf(fields[idx].c, sizeof(fields[0]), fmt, ap) )
+    if( (int)sizeof(ctx->fields[0]) <=
+        vsnprintf(ctx->fields[idx].c, sizeof(ctx->fields[0]), fmt, ap) )
     {
         ERR("Field size exceeded for field '%s'", fieldname);
     }
     va_end(ap);
 }
 
-void _asciilog_emit_record(int Nfields)
+void _asciilog_emit_record(struct asciilog_context_t* ctx, int Nfields)
 {
+    if( ctx == NULL ) ctx = global_context;
+
     if(!legend_finished)
         ERR("need a legend to do this");
 
-    if(!line_has_any_values)
+    if(!ctx->line_has_any_values)
         ERR("Tried to emit a log line without any values being set");
 
     check_fp();
@@ -123,32 +138,15 @@ void _asciilog_emit_record(int Nfields)
     flockfile(fp);
     for(int i=0; i<Nfields-1; i++)
     {
-        _emit(fields[i].c);
+        _emit(ctx->fields[i].c);
         _emit(" ");
     }
-    _emit(fields[Nfields-1].c);
+    _emit(ctx->fields[Nfields-1].c);
     _emit("\n");
     funlockfile(fp);
 
     // I want to be able to process streaming data, so I flush the buffer now
     flush();
 
-    clear(Nfields);
-}
-
-void _asciilog_stash(struct asciilog_context_t* ctx, int Nfields)
-{
-    ctx->line_has_any_values = line_has_any_values;
-    memcpy(ctx->fields, fields, sizeof(fields[0])*Nfields);
-
-    clear(Nfields);
-}
-
-void _asciilog_stash_apply(const struct asciilog_context_t* ctx, int Nfields)
-{
-    if(line_has_any_values)
-        ERR("Tried to apply a stash, but I already have a record in progress");
-
-    line_has_any_values = ctx->line_has_any_values;
-    memcpy(fields, ctx->fields, sizeof(fields[0])*Nfields);
+    _asciilog_clear_ctx(ctx, Nfields);
 }
