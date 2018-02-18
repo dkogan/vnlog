898dfd0b0562280271ba526279ca23e7e731283e
Dima Kogan <dima@secretsauce.net>
2018-02-17 17:18:28 -0800

    minor test data update
    This just tweaks the test data for vnl-join. Doesn't matter right now, but will
    be important for a upcoming patch

--- test/test_vnl-join.pl
+++ test/test_vnl-join.pl
@@ -20,11 +20,11 @@ my $data1 = <<'EOF';
 ## asdf
 # a b e
 ## asdf
-1a 22b 5e
+1a 22b 5
 # asdf
-5a 32b 4e
+5a 32b 4
 ## zxcv
-6a 42b 7e
+6a 42b 7
 EOF
 
 my $data2 = <<'EOF';
@@ -32,17 +32,17 @@ my $data2 = <<'EOF';
 ## zxcv
 # b c d e
 ## zxcv
-22b 1c 5d 1e
+22b 1c 5d 1
 # asdf
-32b 5c 6d 2e
+32b 5c 6d 20
 ## zxcv
-52b 6c 7d 3e
+52b 6c 7d 3
 EOF
 
 
 test_init('vnl-join', \$Nfailed,
-          '$data1'       => $data1,
-          '$data2'       => $data2);
+          '$data1'    => $data1,
+          '$data2'    => $data2);
 
 
 
@@ -61,96 +61,96 @@ check( 'ERROR', qw(-1 b -2 b -j b), '$data1', '$data2' );
 
 check( <<'EOF', qw(-1 b -2 b), '$data1', '$data2' );
 # b a e c d e
-22b 1a 5e 1c 5d 1e
-32b 5a 4e 5c 6d 2e
+22b 1a 5 1c 5d 1
+32b 5a 4 5c 6d 20
 EOF
 
 check( <<'EOF', qw(-1 b -2 b --vnl-prefix1 aaa --vnl-suffix2 bbb), '$data1', '$data2' );
 # b aaaa aaae cbbb dbbb ebbb
-22b 1a 5e 1c 5d 1e
-32b 5a 4e 5c 6d 2e
+22b 1a 5 1c 5d 1
+32b 5a 4 5c 6d 20
 EOF
 
 check( <<'EOF', qw(-1b -2b), '$data1', '$data2' );
 # b a e c d e
-22b 1a 5e 1c 5d 1e
-32b 5a 4e 5c 6d 2e
+22b 1a 5 1c 5d 1
+32b 5a 4 5c 6d 20
 EOF
 
 check( <<'EOF', qw(-j b), '$data1', '$data2' );
 # b a e c d e
-22b 1a 5e 1c 5d 1e
-32b 5a 4e 5c 6d 2e
+22b 1a 5 1c 5d 1
+32b 5a 4 5c 6d 20
 EOF
 
 check( <<'EOF', qw(-j b), '$data1', '-$data2' );
 # b a e c d e
-22b 1a 5e 1c 5d 1e
-32b 5a 4e 5c 6d 2e
+22b 1a 5 1c 5d 1
+32b 5a 4 5c 6d 20
 EOF
 
 check( <<'EOF', qw(-j b), '-$data1', '$data2' );
 # b a e c d e
-22b 1a 5e 1c 5d 1e
-32b 5a 4e 5c 6d 2e
+22b 1a 5 1c 5d 1
+32b 5a 4 5c 6d 20
 EOF
 
 check( <<'EOF', qw(-j b), '-$data1', '$data2' );
 # b a e c d e
-22b 1a 5e 1c 5d 1e
-32b 5a 4e 5c 6d 2e
+22b 1a 5 1c 5d 1
+32b 5a 4 5c 6d 20
 EOF
 
 check( <<'EOF', qw(-jb -a1), '-$data1', '$data2' );
 # b a e c d e
-22b 1a 5e 1c 5d 1e
-32b 5a 4e 5c 6d 2e
-42b 6a 7e - - -
+22b 1a 5 1c 5d 1
+32b 5a 4 5c 6d 20
+42b 6a 7 - - -
 EOF
 
 check( <<'EOF', qw(-jb -a2), '-$data1', '$data2' );
 # b a e c d e
-22b 1a 5e 1c 5d 1e
-32b 5a 4e 5c 6d 2e
-52b - - 6c 7d 3e
+22b 1a 5 1c 5d 1
+32b 5a 4 5c 6d 20
+52b - - 6c 7d 3
 EOF
 
 check( <<'EOF', qw(-jb -a2 -a1), '$data1', '$data2' );
 # b a e c d e
-22b 1a 5e 1c 5d 1e
-32b 5a 4e 5c 6d 2e
-42b 6a 7e - - -
-52b - - 6c 7d 3e
+22b 1a 5 1c 5d 1
+32b 5a 4 5c 6d 20
+42b 6a 7 - - -
+52b - - 6c 7d 3
 EOF
 
 check( <<'EOF', qw(-jb -a1 -a2), '$data1', '$data2' );
 # b a e c d e
-22b 1a 5e 1c 5d 1e
-32b 5a 4e 5c 6d 2e
-42b 6a 7e - - -
-52b - - 6c 7d 3e
+22b 1a 5 1c 5d 1
+32b 5a 4 5c 6d 20
+42b 6a 7 - - -
+52b - - 6c 7d 3
 EOF
 
 check( <<'EOF', qw(-jb -v1), '$data1', '$data2' );
 # b a e c d e
-42b 6a 7e - - -
+42b 6a 7 - - -
 EOF
 
 check( <<'EOF', qw(-jb -v2), '-$data1', '$data2' );
 # b a e c d e
-52b - - 6c 7d 3e
+52b - - 6c 7d 3
 EOF
 
 check( <<'EOF', qw(-jb -v1 -v2), '$data1', '$data2' );
 # b a e c d e
-42b 6a 7e - - -
-52b - - 6c 7d 3e
+42b 6a 7 - - -
+52b - - 6c 7d 3
 EOF
 
 check( <<'EOF', qw(-jb -o),  '1.a,0,2.d,1.e,0,2.e', '$data1', '$data2' );
 # a b d e b e
-1a 22b 5d 5e 22b 1e
-5a 32b 6d 4e 32b 2e
+1a 22b 5d 5 22b 1
+5a 32b 6d 4 32b 20
 EOF
 
 check( 'ERROR', qw(-jb -o),  '5.a,0,2.d,1.e,0,2.e', '$data1', '$data2' );
