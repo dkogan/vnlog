6c1e1e5142a41c9a6820e4ff94ba15f6fbcb4684
Dima Kogan <dima@secretsauce.net>
2018-03-04 00:50:16 -0800

    Python parser: more docs, tests
    This is now done

--- lib/vnlog.py
+++ lib/vnlog.py
@@ -3,15 +3,122 @@
 import re
 import numpy as np
 
+r'''A simple parser for vnlog data
+
+Synopsis:
+
+    import vnlog
+    for d in vnlog.vnlog(f):
+        print d['time'],d['height']
+
+Vnlog is simple, and you don't NEED a parser to read it, but this makes it a bit
+nicer.
+
+This module provides three different ways to parse vnlog
+
+1. slurp the whole thing into a numpy array: the slurp() function. Basic usage:
+
+   import vnlog
+   log_numpy_array,list_keys,dict_key_index = \
+        vnlog.slurp(filename_or_fileobject)
+
+   This parses out the legend, and then calls numpy.loadtxt(). Null data values
+   ('-') are not supported
+
+2. Iterate through the records: vnlog class, used as an iterator. Basic usage:
+
+   import vnlog
+   for d in vnlog.vnlog(filename_or_fileobject):
+       print d['time'],d['height']
+
+   Null data values are represented as None
+
+
+3. Parse incoming lines individually: vnlog class, using the parse() method.
+   Basic usage:
+
+   import vnlog
+   parser = vnlog.vnlog()
+   for l in file:
+       parser.parse(l)
+       d = parser.values_dict()
+       if not d:
+           continue
+       print d['time'],d['height']
+
+Most of the time you'd use options 1 or 2 above. Option 3 is the most general,
+but also the most verbose
+
+'''
+
 
 class vnlog:
 
-    def __init__(self):
+    r'''Class to facilitate vnlog parsing
+
+    This class provides two different ways to parse vnlog
+
+    1. Iterate through the records: vnlog class, used as an iterator. Basic
+       usage:
+
+       import vnlog
+       for d in vnlog.vnlog(filename_or_fileobject):
+           print d['time'],d['height']
+
+       Null data values are represented as None
+
+
+    2. Parse incoming lines individually: vnlog class, using the parse() method.
+       Basic usage:
+
+       import vnlog
+       parser = vnlog.vnlog()
+       for l in file:
+           parser.parse(l)
+           d = parser.values_dict()
+           if not d:
+               continue
+           print d['time'],d['height']
+
+    '''
+
+    def __init__(self, f = None):
+        r'''Initialize the vnlog parser
+
+        If using this class as an iterator, you MUST pass a filename or file
+        object into this constructor
+        '''
+
         self._keys        = None
         self._values      = None
         self._values_dict = None
 
+        if f is None or type(f) is not str:
+            self.f = f
+            self.f_need_close = False
+        else:
+            self.f = open(f, 'r')
+            self.f_need_close = True
+
+    def __del__(self):
+        try:
+            if self.f_need_close:
+                self.f.close()
+        except:
+            pass
+
     def parse(self, l):
+        r'''Parse a new line of data.
+
+        The user only needs to call this if they're not using this class as an
+        iterator. When this function returns, the keys(), values() and
+        values_dict() functions return the data from this line. Before the
+        legend was parsed, all would return None. After the legend was parsed,
+        keys() returns non-None. When a comment is encountered, values(),
+        values_dict() return None
+
+        '''
+
         # I reset the data first
         self._values      = None
         self._values_dict = None
@@ -56,12 +163,25 @@ class vnlog:
         return True
 
     def keys(self):
+        r'''Returns the keys of the so-far-parsed data
+
+        Returns None if we haven't seen the legend line yet'''
         return self._keys
 
     def values(self):
+        r'''Returns the values list of the last-parsed line
+
+        Returns None if the last line was a comment. Null fields ('-') values
+        are represented as None
+        '''
         return self._values
 
     def values_dict(self):
+        r'''Returns the values dict of the last-parsed line
+
+        This dict maps field names to values. Returns None if the last line was
+        a comment. Null fields ('-') values are represented as None.
+        '''
 
         # internally:
         #   self._values_dict == None:  not yet computed
@@ -80,9 +200,75 @@ class vnlog:
         return self._values_dict
 
 
+    def __iter__(self):
+        if self.f is None:
+            raise Exception("Cannot iterate since this vnlog instance was not given a log to iterate on")
+        return self
+
+    def __next__(self):
+        for l in self.f:
+            self.parse(l)
+            if self._values is None:
+                continue
+
+            return self.values_dict()
+        raise StopIteration
+
+    # to support python2 and python3
+    next = __next__
 
 
-# basic usage
+def _slurp(f):
+    r'''Reads a whole vnlog into memory
+
+    This is an internal function. The argument is a file object, not a filename.
+
+    Returns a tuple (log_numpy_array, list_keys, dict_key_index)
+
+    '''
+
+    parser = vnlog()
+
+    for l in f:
+        parser.parse(l)
+        keys = parser.keys()
+        if keys is not None:
+            break
+
+    dict_key_index = {}
+    for i in xrange(len(keys)):
+        dict_key_index[keys[i]] = i
+
+    return np.loadtxt(f),keys,dict_key_index
+
+
+def slurp(f):
+    r'''Reads a whole vnlog into memory
+
+    Synopsis:
+
+        import vnlog
+        log_numpy_array,list_keys,dict_key_index = \
+             vnlog.slurp(filename_or_fileobject)
+
+        This parses out the legend, and then calls numpy.loadtxt(). Null data
+        values ('-') are not supported.
+
+    Returns a tuple (log_numpy_array, list_keys, dict_key_index)
+
+    '''
+
+    if type(f) is str:
+        with open(f, 'r') as fh:
+            return _slurp(fh)
+    else:
+        return _slurp(f)
+
+
+
+
+
+# Basic usage. More examples in test_python_parser.py
 if __name__ == '__main__':
 
     import cStringIO
@@ -99,12 +285,5 @@ if __name__ == '__main__':
 7 8
 ''')
 
-    parser = vnlog()
-
-    for l in f:
-        parser.parse(l)
-        d = parser.values_dict()
-        if not d:
-            continue
-
+    for d in vnlog(f):
         print d['time'],d['height']
--- test/test_python_parser.py
+++ test/test_python_parser.py
@@ -2,13 +2,14 @@
 
 import os
 import sys
+import numpy as np
 
 sys.path[:0] = (os.path.abspath(os.path.dirname(sys.argv[0])) + "/../lib",)
 
 import vnlog
 import cStringIO
 
-f = cStringIO.StringIO('''#! zxcv
+inputstring = '''#! zxcv
 # time height
 ## qewr
 1 2
@@ -18,19 +19,7 @@ f = cStringIO.StringIO('''#! zxcv
 6 -
 - -
 7 8
-''')
-
-parser = vnlog.vnlog()
-resultstring = ''
-for l in f:
-    parser.parse(l)
-    d = parser.values_dict()
-    if not d:
-        continue
-
-    resultstring += '{} {}\n'.format(d['time'],d['height'])
-
-
+'''
 
 ref = r'''1 2
 3 4
@@ -40,10 +29,60 @@ None None
 7 8
 '''
 
-if resultstring == ref:
-    print "Test passed";
-    sys.exit(0);
+f = cStringIO.StringIO(inputstring)
 
-print "Expected '{}' but got '{}'".format(ref, resultstring)
-print "Test failed!"
-sys.exit(1)
+parser = vnlog.vnlog()
+resultstring = ''
+for l in f:
+    parser.parse(l)
+    d = parser.values_dict()
+    if not d:
+        continue
+    resultstring += '{} {}\n'.format(d['time'],d['height'])
+
+if resultstring != ref:
+    print "Expected '{}' but got '{}'".format(ref, resultstring)
+    print "Test failed!"
+    sys.exit(1)
+
+
+
+
+f = cStringIO.StringIO(inputstring)
+
+resultstring = ''
+for d in vnlog.vnlog('/tmp/tst.vnl'):
+    resultstring += '{} {}\n'.format(d['time'],d['height'])
+if resultstring != ref:
+    print "Expected '{}' but got '{}'".format(ref, resultstring)
+    print "Test failed!"
+    sys.exit(1)
+
+
+
+inputstring_noundef = r'''#! zxcv
+# time height
+## qewr
+1 2
+3 4
+# - 10
+7 8
+'''
+ref_noundef = np.array(((1,2),(3,4),(7,8)))
+f = cStringIO.StringIO(inputstring_noundef)
+log_numpy_array,list_keys,dict_key_index = vnlog.slurp(f)
+if np.linalg.norm((ref_noundef - log_numpy_array).ravel()) > 1e-8:
+    raise Exception("Array mismatch: expected '{}' but got '{}". \
+                    format(ref_noundef, log_numpy_array))
+if len(list_keys) != 2 or list_keys[0] != 'time' or list_keys[1] != 'height':
+    raise Exception("Key mismatch: expected '{}' but got '{}". \
+                    format(('time','height'), list_keys))
+if len(dict_key_index) != 2 or dict_key_index['time'] != 0 or dict_key_index['height'] != 1:
+    raise Exception("Key-dict mismatch: expected '{}' but got '{}". \
+                   format({'time': 0, 'height': 1}, dict_key_index))
+
+
+
+print "Test passed";
+sys.exit(0);
+
