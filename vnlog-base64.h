72ec4083fbc50f0ebe1bdd5cc693aec5bfe3497c
Dima Kogan <dima@secretsauce.net>
2019-07-16 14:35:56 -0700

    exported vnlog-flavored base64 encoder

--- vnlog-base64.h
+++ vnlog-base64.h
@@ -8,10 +8,10 @@
 //
 // The number of bytes in the output (not including the trailing '\0') is
 // returned on success, or <0 on error
-int base64_encode(       char* dst, int dstlen,
-                   const char* src, int srclen );
+int vnlog_base64_encode(       char* dst, int dstlen,
+                         const char* src, int srclen );
 
-static inline int base64_dstlen_to_encode( int len )
+static inline int vnlog_base64_dstlen_to_encode( int len )
 {
     // + 1 for the trailing '\0'
     return (1 + (len-1)/3) * 4 + 1;
