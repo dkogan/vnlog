72f67bfc1fac9d7332c787290557132288ab415a
Dima Kogan <dima@secretsauce.net>
2023-06-19 12:31:04 -0700

    more extensive C parser test

--- test/test_c_api.sh
+++ test/test_c_api.sh
@@ -47,6 +47,40 @@ z = 53
 query: y = 25
 EOF
 
+read -r -d '' ref_y_gap_first_row <<'EOF' || true
+======
+time = 0
+id = abc
+x = 1
+y = -
+z = -
+query: y = -
+======
+time = 1
+id = def
+x = 11
+y = 25
+z = 53
+query: y = 25
+EOF
+
+read -r -d '' ref_y_gap_second_row <<'EOF' || true
+======
+time = 0
+id = abc
+x = 1
+y = 5
+z = 3
+query: y = 5
+======
+time = 1
+id = def
+x = 11
+y = -
+z = -
+query: y = -
+EOF
+
 echo '
 # time id x y z
 0 abc 1 5 3
@@ -55,6 +89,32 @@ echo '
 
 diff -q test-parser.got <(echo "$ref_y") >&/dev/null || { echo "LINE $LINENO: mismatched output!"; exit 1; }
 
+echo '
+# time id x y z # asdf err
+0 abc 1 5 3 # 115 113
+## zxvvvv
+1 def 11 25 53
+' | ./test-parser - y 2>/dev/null > test-parser.got || { echo "LINE $LINENO: FAILED!"; exit 1; }
+
+diff -q test-parser.got <(echo "$ref_y") >&/dev/null || { echo "LINE $LINENO: mismatched output!"; exit 1; }
+
+echo '
+# time id x y z # asdf err
+0 abc 1 - - # 115 113
+## zxvvvv
+1 def 11 25 53
+' | ./test-parser - y 2>/dev/null > test-parser.got || { echo "LINE $LINENO: FAILED!"; exit 1; }
+
+diff -q test-parser.got <(echo "$ref_y_gap_first_row") >&/dev/null || { echo "LINE $LINENO: mismatched output!"; exit 1; }
+
+echo '
+# time id x y z # asdf err
+0 abc 1 5 3 # 115 113
+## zxvvvv
+1 def 11 - -
+' | ./test-parser - y 2>/dev/null > test-parser.got || { echo "LINE $LINENO: FAILED!"; exit 1; }
+
+diff -q test-parser.got <(echo "$ref_y_gap_second_row") >&/dev/null || { echo "LINE $LINENO: mismatched output!"; exit 1; }
 
 echo '
 ## adsf
